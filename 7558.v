aee3c73f3ac0e3eff5df8c728186ff8ad31081b0
Sheikh Fahad <70671788+nameer1811@users.noreply.github.com>
2021-10-14 23:33:07 -0500

    Create 7558.v

---
+++ 7558.v
@@ -0,0 +1,18 @@
+module top_module ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+    
+    wire wireAndA, wireAndB, wireAndC, wireAndD;
+    
+    assign wireAndA = p1a && p1c && p1b;
+    assign wireAndB = p1f && p1e && p1d;
+    assign wireAndC = p2a && p2b;
+    assign wireAndD = p2c && p2d;
+    
+    assign p1y = wireAndA || wireAndB;
+    assign p2y = wireAndC || wireAndD;
+
+
+endmodule
